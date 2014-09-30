244834dd708dc693c975009bd4d8b0e92a6b737e
tchapi <regbasket@gmail.com>
2014-09-30 18:04:32 +0000

    Fixups

--- models/SNESGPIOController.py
+++ models/SNESGPIOController.py
@@ -4,6 +4,7 @@ class SNESGPIOController:
 
   # Pinout
   KEY_1 = 21
+  KEY_2 = 13
 
   def __init__(self, app, snesController):
     self.app = app
@@ -11,18 +12,23 @@ class SNESGPIOController:
     GPIO.setmode(GPIO.BCM)
 
 # FOR EACH PIN
-    # Flex Sensor : needs PULLUP with other side to GND
+    # FORCE Flex Sensor : needs PULLUP with other side to GND
     GPIO.setup(SNESGPIOController.KEY_1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
     GPIO.add_event_detect(SNESGPIOController.KEY_1, GPIO.RISING, callback=self.detect, bouncetime=300)
+    # MEAS PIEZO Sensor : needs PULLDOWN with no other side
+    GPIO.setup(SNESGPIOController.KEY_2, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.add_event_detect(SNESGPIOController.KEY_2, GPIO.RISING, callback=self.detect, bouncetime=300)
 # ENDFOR
 
 # Will need to be extended for combos...
   def chooseCommandFromChannel(self, channel):
     if (channel == SNESGPIOController.KEY_1):
       return "A"
+    if (channel == SNESGPIOController.KEY_2):
+      return "B"
 
   def detect(self,channel):
-    print("Channel %s activated" % channel)
+    print("Pin %s activated" % channel)
     self.snesController.sendSNESCommand(self.chooseCommandFromChannel(channel))
 
 
