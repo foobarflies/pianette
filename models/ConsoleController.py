cfcec24f032970d6cf5cb9ac6907eb5424acb380
Eric Redon <eric.redon@coox.org>
2014-10-11 19:21:04 +0200

    properly add bits

--- models/ConsoleController.py
+++ models/ConsoleController.py
@@ -89,18 +89,18 @@ class ConsoleController:
     self.state_as_bytes = [ 0xFF, 0x41, 0x5A ] # Controller ID
 
     self.state_as_bytes.append( # Data byte 1
-      (self.stateController.state["SELECT"] & 0b00000001) &
-      (self.stateController.state["START"]  & 0b00001000) &
-      (self.stateController.state["TOP"]    & 0b00010000) &
-      (self.stateController.state["RIGHT"]  & 0b00100000) &
-      (self.stateController.state["BOTTOM"] & 0b01000000) &
+      (self.stateController.state["SELECT"] & 0b00000001) |
+      (self.stateController.state["START"]  & 0b00001000) |
+      (self.stateController.state["TOP"]    & 0b00010000) |
+      (self.stateController.state["RIGHT"]  & 0b00100000) |
+      (self.stateController.state["BOTTOM"] & 0b01000000) |
       (self.stateController.state["LEFT"]   & 0b10000000)
     )
 
     self.state_as_bytes.append( # Data byte 2
-      (self.stateController.state["T"] & 0b00010000) &
-      (self.stateController.state["O"] & 0b00100000) &
-      (self.stateController.state["X"] & 0b01000000) &
+      (self.stateController.state["T"] & 0b00010000) |
+      (self.stateController.state["O"] & 0b00100000) |
+      (self.stateController.state["X"] & 0b01000000) |
       (self.stateController.state["S"] & 0b10000000)
     )
 
