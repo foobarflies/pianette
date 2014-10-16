7e5ec21e5fcd67f7b11ba699b9369c0e513a2ff8
Eric Redon <eric.redon@coox.org>
2014-10-16 14:27:52 +0200

    refactor GPIO module, part one

--- models/GPIOController.py
+++ models/GPIOController.py
@@ -2,43 +2,45 @@ from utils import *
 
 import RPi.GPIO as GPIO
 
+"""
+# Global GPIO Pin to Piano Note Mapping
+"""
+PIANO_NOTE_FOR_GPIO_PIN = {   
+    # Bank 1, Octave -1
+    5: "LEFT",    # Do
+    6: "LEFT2",   # Ré
+    13: "CL",     # Mi
+    19: "DOWN",   # Fa
+    26: "CL2",    # Sol
+
+    # Bank 2, Octave -1
+    21: "RIGHT",  # La
+    20: "CL3",    # Sib
+    16: "RIGHT2", # Si
+    12: "CL4",    # Do
+    25: None,
+
+    # Bank 3, Octave 1
+    24: "S",      # Do
+    23: "O",      # Ré
+    18: "CR",     # Mib
+    15: "X",      # Mi
+    14: "S2",     # Fa
+
+    # Bank 4, Octave 1
+    2: "CR2",     # Solb
+    3: "O2",      # Sol
+    4: "T",       # La
+    17: "CR3",    # Sib
+    27: "T2",     # Si
+
+    # N/A : "SELECT",
+    # N/A : "START",
+}
+
 class GPIOController:
 
-  # Mapping
-  KEYS  = {   
-            # Bank 1, Octave -1
-            5: "LEFT",    # Do
-            6: "LEFT2",   # Ré
-            13: "CL",     # Mi
-            19: "DOWN",   # Fa
-            26: "CL2",    # Sol
-
-            # Bank 2, Octave -1
-            21: "RIGHT",  # La
-            20: "CL3",    # Sib
-            16: "RIGHT2", # Si
-            12: "CL4",    # Do
-            25: None,
-
-            # Bank 3, Octave 1
-            24: "S",      # Do
-            23: "O",      # Ré
-            18: "CR",     # Mib
-            15: "X",      # Mi
-            14: "S2",     # Fa
-
-            # Bank 4, Octave 1
-            2: "CR2",     # Solb
-            3: "O2",      # Sol
-            4: "T",       # La
-            17: "CR3",    # Sib
-            27: "T2",     # Si
-
-            # N/A : "SELECT",
-            # N/A : "START",
-          }
-
-  # GPIO 22 is used for the reset key
+  # GPIO Pin 22 is used for the reset key
   RESET_KEY = 22
 
   def __init__(self, stateController, app):
@@ -54,7 +56,7 @@ class GPIOController:
     GPIO.add_event_detect(self.RESET_KEY, GPIO.FALLING, callback=self.gpio_reset, bouncetime=300)
 
     ## Attach a callback to each INPUT pin
-    for pin, button in self.KEYS.items():
+    for pin, button in PIANO_NOTE_FOR_GPIO_PIN.items():
       Debug.println("SUCCESS", "Pin %s => %s" % (pin, button) )
       try:
         GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
@@ -76,7 +78,7 @@ class GPIOController:
   # Callback for all action pins = keys
   def gpio_callback(self,channel):
 
-    command = self.KEYS[channel]
+    command = PIANO_NOTE_FOR_GPIO_PIN[channel]
 
     Debug.println("INFO", "Pin %s activated : Button %s" % (channel, command) )
     self.stateController.raiseFlag(command)
