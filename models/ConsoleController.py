79be4ff1876fca4ab805e391167a4de9484092bf
tchapi <regbasket@gmail.com>
2014-10-20 21:48:59 +0200

    Cleanup

--- models/ConsoleController.py
+++ models/ConsoleController.py
@@ -5,8 +5,6 @@ import serial
 import time
 import random
 
-usleep = lambda x: time.sleep(x/1000000.0)
-
 # Number of µsecs that we need to wait between commands from controller
 usecs_between_data = 1
 
@@ -44,98 +42,6 @@ class ConsoleController:
         except serial.SerialException:
             pass
     return result
-
-  # Single buttons
-  def sendX(self):
-    self.serialConnection.write(b'\xFF\xBF')
-  def sendO(self):
-    self.serialConnection.write(b'\xFF\xDF')
-  def sendSquare(self):
-    self.serialConnection.write(b'\xFF\x7F')
-  def sendTriangle(self):
-    self.serialConnection.write(b'\xFF\xEF')
-
-  def sendStart(self):
-    self.serialConnection.write(b'\xF7\xFF')
-  def sendSelect(self):
-    self.serialConnection.write(b'\xF7\xFE')
-
-  def sendUp(self):
-    self.serialConnection.write(b'\xEF\xFF')
-  def sendDown(self):
-    self.serialConnection.write(b'\xBF\xFF')
-  def sendLeft(self):
-    self.serialConnection.write(b'\x7F\xFF')
-  def sendRight(self):
-    self.serialConnection.write(b'\xDF\xFF')
-
-  # Simple movement combos
-  def sendDownRight(self):
-    # RIGHT + DOWN
-    self.serialConnection.write(b'\x9F\xFF')
-  def sendDownLeft(self):
-    # LEFT + DOWN
-    self.serialConnection.write(b'\x3F\xFF')
-  def sendUpRight(self):
-    # RIGHT + UP
-    self.serialConnection.write(b'\xCF\xFF')
-  def sendUpLeft(self):
-    # LEFT + UP
-    self.serialConnection.write(b'\x6F\xFF')
-
-  # Send a reset byte couple, going back to the menus
-  def sendReset(self):
-    self.serialConnection.write(b'\x00\x00')
-
-  # Methods 
-  def restartFresh(self):
-    # Back to main menu
-    self.sendReset()
-    usleep(usecs_between_data)
-    self.sendStart()
-    usleep(usecs_between_data)
-    # Choose "VERSUS" mode
-    self.chooseVersusModeFromMenu()
-    self.newGameFromVersusMenu()
-    
-  def restartSuperFresh(self):
-    # Back to start
-    self.sendReset()
-    usleep(usecs_between_data)
-    self.restartFresh()
-    
-  def chooseVersusModeFromMenu(self):
-    self.sendLeft()
-    usleep(usecs_between_data)
-    self.sendX()
-    usleep(usecs_between_data)
-
-  def newGameFromVersusMenu(self):
-    # Choose random character
-    if (self.psx_controller_state.playerOne == True):
-      self.sendRight()
-    else:
-      self.sendLeft()
-    usleep(usecs_between_data)
-    self.sendUp()
-    usleep(usecs_between_data)
-    self.sendUp()
-    usleep(usecs_between_data)
-    # Acknowledge handicap
-    self.sendX()
-    usleep(usecs_between_data)
-    # Acknowledge Battle field - we choose a random stage going right >>
-    stage = random.randint(1, 10)
-    for x in xrange(1,stage):
-      self.sendRight()
-      usleep(usecs_between_data)
-    self.sendX()
-    usleep(usecs_between_data)
-
-    # Wait two seconds for cinematic
-    time.sleep(2)
-
-    # FIGHT !!!!
 
   def sendStateBytes(self):
     stateByte1 = (
