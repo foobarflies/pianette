b606d7b0a7a1400b3103543bd0e88f8406f76f81
tchapi <regbasket+github@gmail.com>
2016-09-03 17:29:09 +0200

    Add an init script

---
+++ pianette_initd_script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+case "$1" in
+    start)
+        sudo PYTHONIOENCODING="utf-8" /home/pi/pianette/main.py
+    ;;
+
+    stop)
+        sudo killall python3
+    ;;
+
+    *)
+        echo 'Usage: /etc/init.d/pianette {start|stop}'
+        exit 1
+    ;;
+esac
+
+exit 0
